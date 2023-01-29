bd63257758545605a7156e449c5dee32003cd361
a3r0 <nitusingh14372@gmail.com>
2023-01-29 15:35:48 +0530

    almost finished

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ bank = banking_features
 
 print("Welcome to the ATM Service")
 greet_fact = fact.random_Banking_fact
-print(f"Fact: {greet_fact}")
+print(f"Fun Fact: {greet_fact}")
 
 print("Connecting to the Gigachad Bank Of Erdia....")
 print("--------------------------------------------")
@@ -20,6 +20,17 @@ print("--------------------------------------------")
 
 class ATM:
 
+    @staticmethod
+    def menu():
+        menu = """
+At Gigachad Bank of Erdia, managing your finances has never been easier. With our user-friendly online banking platform, you can:
+1. Securely access your account and view your balance.
+    2. Open a new account and start managing your money."""
+
+        print(menu)
+        global menu_choice
+        menu_choice = int(input("Enter your choice: "))
+    
     @staticmethod
     def __init__():
         print(err_succ.success_or_error)
@@ -36,33 +47,49 @@ class ATM:
 
             print("Success in establishing connection to the Gigachad Bank Of Erdia...")
             print("----------------------------------------------------------------------------------")
+            ATM.menu()
 
-            menu = """
-At Gigachad Bank of Erdia, managing your finances has never been easier. With our user-friendly online banking platform, you can:
-1. Securely access your account and view your balance.
-    2. Open a new account and start managing your money.
-        3. Monitor your account activity and track your spending.
-            4. Easily withdraw funds from your account at any time."""
-
-            print(menu)
-
-            menu_choice = int(input("Enter your choice: "))
             if menu_choice == 1:
                 usrnm = str(input("Username: "))
                 paswd = int(input("Password: "))
                 auth.login(username=usrnm, password=paswd)
+                print("Logged in successfully")
+                print(f"Balance: {bank.view_balance(username=usrnm)}")
+                menu_ = f"""
+Succesfully logged in At Gigachad Bank of Erdia as {usrnm},  you can:
+        1. Monitor your account activity and track your spending.
+            2. Easily deposit funds in your account anytime.
+                3. Withdraw funds from you account anytime."""
+
+                print(menu_)
+
+                menu_choice_ = int(input("Enter your choice: "))
+    
             elif menu_choice == 2:
                 usrnm = str(input("Username: "))
                 paswd = int(input("Password: "))
                 auth.user_setup(username=usrnm, password=paswd)
-            elif menu_choice == 3:
+            elif menu_choice_ == 1:
                 usrnm = str(input("Username: "))
                 paswd = int(input("Password: "))
                 bal = bank.view_balance(username=usrnm)
+                print(f"Current balance: {bal}")
                 with open(f"db/user_logs/{usrnm}.txt", 'r') as log:
                     print(log.read())
-            
+            elif menu_choice_ == 2:
+                print(f"Username: {usrnm}\nBalance: {bal}")
+                dep_val = int(input("Deposit Amount: "))
+                bank.deposit(username=usrnm, value=dep_val)
+                bal_updated = bank.view_balance(username=usrnm)
+                print(f"Current balance: {bal_updated}")
+            elif menu_choice_ == 3:
+                print(f"Username: {usrnm}\nBalance: {bal}")
+                withd_val = int(input("Withdraw Amount: "))
+                bank.withdraw(username=usrnm, value=withd_val)
+                bal_after_withdraw = bank.view_balance(username=usrnm)
+                print(f"Current balance: {bal_after_withdraw}")
 
 
 
-                ATM()
+
+ATM()
